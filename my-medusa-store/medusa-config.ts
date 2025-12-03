5fb2b2591e658eb43c8e8dcf1fd44dbbb21cd412
RenaldiJ88 <jeros_lp@hotmail.com>
2025-12-03 16:57:45 -0300

    Fix product page build error v1.2

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -5,7 +5,10 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    // ESTA SECCIÓN 'http' ES OBLIGATORIA
+    // --- CAMBIOS AGREGADOS AQUÍ ---
+    redisUrl: process.env.REDIS_URL,
+    workerMode: "shared",
+    // -----------------------------
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -16,14 +19,13 @@ module.exports = defineConfig({
   },
   modules: [
     {
-      resolve: "@medusajs/medusa/payment", // O "@medusajs/payment" dependiendo de la versión exacta de la beta/rc
+      resolve: "@medusajs/medusa/payment",
       options: {
         providers: [
           {
             resolve: "./src/services/mercadopago-provider.ts",
             id: "mercadopago",
             options: {
-               // Las credenciales aquí
                access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
                public_key: process.env.MERCADOPAGO_PUBLIC_KEY,
             },
